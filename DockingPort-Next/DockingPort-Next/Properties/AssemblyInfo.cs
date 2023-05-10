347b124ebec9284103d21df5815ece72daf95ce3
meirumeiru <33137857+meirumeiru@users.noreply.github.com>
2023-05-10 15:49:59 +0200

    preparing release

--- DockingPort-Next/DockingPort-Next/Properties/AssemblyInfo.cs
+++ DockingPort-Next/DockingPort-Next/Properties/AssemblyInfo.cs
@@ -36,4 +36,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.1.0")]
 
 
-[assembly: KSPAssemblyDependency("AttachmentAndDockingTools", 1, 0)]
+[assembly: KSPAssemblyDependency("DockingFunctions", 1, 0)]
