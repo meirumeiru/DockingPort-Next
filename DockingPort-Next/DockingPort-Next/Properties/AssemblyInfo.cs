1522f9fb5bacbbc8a64537ad23565bee7afef288
meirumeiru <33137857+meirumeiru@users.noreply.github.com>
2023-08-29 00:31:10 +0200

    version

--- DockingPort-Next/DockingPort-Next/Properties/AssemblyInfo.cs
+++ DockingPort-Next/DockingPort-Next/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
 
 
 [assembly: KSPAssemblyDependency("DockingFunctions", 1, 0)]
